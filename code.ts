b01d43b096af5c41daf0546d4ea4b49af0210930
Marcelo Brasileiro <marcelo.brasileirovale@gmail.com>
2020-10-05 16:08:55 -0300

    Switched lots of if statements for switch statemen

--- code.ts
+++ code.ts
@@ -16,27 +16,47 @@ if (figma.command == "copy") {
     for (let node of copy) {
       let findNode = figma.getNodeById(node.id) as SceneNode;
       if (findNode) {
-        if (findNode.type == "COMPONENT") {
-          var instance = findNode.createInstance();
-          if (instance.parent == figma.currentPage) {
-            instance.x = destination.width / 2;
-            instance.y = destination.height / 2;
-            destination.appendChild(instance);
-          } else {
-            destination.appendChild(instance);
-          }
-        } else {
-          var clone = findNode.clone();
-          if (clone.parent == figma.currentPage) {
-            clone.x = destination.width / 2;
-            clone.y = destination.height / 2;
-            destination.appendChild(clone);
-          } else {
-            destination.appendChild(clone);
-          }
+        (findNode.parent.type);
+        switch (findNode.type) {
+          case "COMPONENT":
+            ("Fzer instancia");
+            var instance = findNode.createInstance();
+            switch (findNode.parent.type) {
+              case "PAGE":
+                ("filho da pagina");
+                instance.x = 0;
+                instance.y = 0;
+                destination.appendChild(instance);
+                break;
+              default:
+                ("Não é filho da página");
+                instance.x = findNode.x;
+                instance.y = findNode.y;
+                destination.appendChild(instance);
+            }
+            break;
+          default:
+            ("Elemento normal");
+            let clone = findNode.clone();
+            (clone.parent.type);
+            switch (findNode.parent.type) {
+              case "PAGE":
+                ("Normal filho da pagina");
+                clone.x = 0;
+                clone.y = 0;
+                destination.appendChild(clone);
+                break;
+              default:
+                ("Normal, nao é filho da página");
+                clone.x = findNode.x;
+                clone.y = findNode.y;
+                destination.appendChild(clone);
+            }
         }
-      }else{
-        figma.closePlugin("Original layer was deleted. Don't delete a layer before you paste it!")
+      } else {
+        figma.closePlugin(
+          "Some layer was deleted, therefore wasn't pasted. Don't delete a layer before you paste it."
+        );
       }
     }
   }
