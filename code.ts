e4ecb6d6bce2903879eb15f60d9163e8aaedfd08
Marcelo Brasileiro <marcelo.brasileirovale@gmail.com>
2020-10-05 14:09:21 -0300

    Added logic to copy component

--- code.ts
+++ code.ts
@@ -12,18 +12,37 @@ if (figma.command == "copy") {
   } catch (e) {
     figma.closePlugin();
   }
-  console.log(copy);
-  console.log(destinations);
   for (let destination of destinations) {
     for (let node of copy) {
       let findNode = figma.getNodeById(node.id) as SceneNode;
-      var clone = findNode.clone();
-      destination.appendChild(clone);
+      if (findNode) {
+        if (findNode.type == "COMPONENT") {
+          var instance = findNode.createInstance();
+          if (instance.parent == figma.currentPage) {
+            instance.x = destination.width / 2;
+            instance.y = destination.height / 2;
+            destination.appendChild(instance);
+          } else {
+            destination.appendChild(instance);
+          }
+        } else {
+          var clone = findNode.clone();
+          if (clone.parent == figma.currentPage) {
+            clone.x = destination.width / 2;
+            clone.y = destination.height / 2;
+            destination.appendChild(clone);
+          } else {
+            destination.appendChild(clone);
+          }
+        }
+      }else{
+        figma.closePlugin("Original layer was deleted. Don't delete a layer before you paste it!")
+      }
     }
   }
-  figma.closePlugin("Layers pasted!")
+  figma.closePlugin("Layers pasted!");
 } else if (figma.command == "clear") {
-  figma.root.setPluginData('copy', '{}');
+  figma.root.setPluginData("copy", "{}");
   figma.closePlugin("Cache cleared");
 }
 figma.closePlugin();
